ba6f1028aa3e46c78c89eae4e64a09f9fd71b52b
Vincent DURMONT <vdurmont@gmail.com>
2014-09-17 21:52:12 -0700

    First commit: grunt system

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+
+    concat: {
+      options: {
+        separator: ";"
+      },
+      app_js: {
+        src: ["src/userinfo.js"],
+        dest: "dist/tmp.js"
+      },
+    },
+
+    uglify: {
+      options: {
+        // the banner is inserted at the top of the output
+        banner: "/*! <%= pkg.name %> <%= pkg.version %> [<%= grunt.template.today('dd-mm-yyyy') %>] */\n",
+        mangle: false
+      },
+      dist: {
+        files: {
+          "dist/<%= pkg.name %>.<%= pkg.version %>.min.js": ["<%= concat.app_js.dest %>"]
+        }
+      }
+    },
+
+    jshint: {
+      files: ["src/*.js"],
+      options: {
+        globals: {
+          console: true,
+          module: true,
+          smarttabs: false,
+          angular: false
+        }
+      }
+    },
+    clean: {
+      tmp: { src: ["dist/tmp.js"] },
+      all: { src: ["dist/**"] }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+
+  grunt.registerTask("default", ["clean:all", "jshint", "concat", "uglify", "clean:tmp"]);
+};
