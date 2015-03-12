80520614c9a8dabbec2b70cdd9912d808d23c55c
Vincent DURMONT <vdurmont@gmail.com>
2015-03-12 16:28:59 -0700

    Adding a X-Userinfo-Client-Id header to the requests to identify the client

--- src/userinfo.js
+++ src/userinfo.js
@@ -1,46 +1,47 @@
-(function (root, factory) {
+(function(root, factory) {
     if (typeof define === 'function' && define.amd) {
         define(factory);
     } else {
         root.UserInfo = factory();
     }
-}(this, function () {
+}(this, function() {
     var API_URL = "http://api.userinfo.io/userinfos";
 
     return {
-      getInfo: function(success, failure) {
-        var xhr;
+        getInfo: function(success, failure) {
+            var xhr;
 
-        if (window.XMLHttpRequest) {
-          // code for IE7+, Firefox, Chrome, Opera, Safari
-          xhr = new XMLHttpRequest();
-        } else {
-          // code for IE6, IE5
-          xhr = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-
-        xhr.onreadystatechange = function() {
-          if (xhr.readyState == 4 ) {
-            if(xhr.status == 200){
-              if (success) {
-                success(JSON.parse(xhr.responseText));
-              }
+            if (window.XMLHttpRequest) {
+                // code for IE7+, Firefox, Chrome, Opera, Safari
+                xhr = new XMLHttpRequest();
             } else {
-              var err;
-              if (xhr.responseText !== null && xhr.responseText !== "") {
-                err = JSON.parse(xhr.responseText);
-              } else {
-                err = { message: "Error with HTTP status code: " + xhr.status };
-              }
-              if (failure) {
-                failure(err);
-              }
+                // code for IE6, IE5
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
             }
-          }
-        };
 
-        xhr.open("GET", API_URL, true);
-        xhr.send();
-      }
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        if (success) {
+                            success(JSON.parse(xhr.responseText));
+                        }
+                    } else {
+                        var err;
+                        if (xhr.responseText !== null && xhr.responseText !== "") {
+                            err = JSON.parse(xhr.responseText);
+                        } else {
+                            err = { message: "Error with HTTP status code: " + xhr.status };
+                        }
+                        if (failure) {
+                            failure(err);
+                        }
+                    }
+                }
+            };
+
+            xhr.open("GET", API_URL, true);
+            xhr.setRequestHeader("X-Userinfo-Client-Id", "userinfo-js:1.0.0");
+            xhr.send();
+        }
     };
 }));
